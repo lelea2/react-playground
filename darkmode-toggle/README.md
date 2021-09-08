b1d59370ddca4a1db0ac0bc2873fbd531a13c4a4
Khanh Dao <khdao@khdao-mn2.linkedin.biz>
2021-09-07 23:41:46 -0700

    darkmode

---
+++ darkmode-toggle/README.md
@@ -0,0 +1,5 @@
+### Darkmode toggle
+
+- Use createGlobalStyle and ThemeProvider from styled-components library
+- Leverage `prefer-color-scheme` media query for OS darkmode setting, update localStorage for theme managing
+- Create reusable react-hook component for managing darkmode
---
+++ darkmode-toggle/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { useDarkMode } from './useDarkMode';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+import Toggle from './components/Toggle';
+
+function App() {
+  const [theme, toggleTheme, componentMounted] = useDarkMode();
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  if (!componentMounted) {
+    return <div />
+  };
+
+  return (
+    <ThemeProvider theme={themeMode}>
+      <>
+        <GlobalStyles />
+        <Toggle theme={theme} toggleTheme={toggleTheme} />
+        <h1>It's a {theme === 'light' ? 'light theme' : 'dark theme'}!</h1>
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
